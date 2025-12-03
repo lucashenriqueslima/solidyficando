13a5e88a0f1947059c52ec394195b4dcdac2e5e3
Lucas <lucashenriqueslima@outlook.com>
2025-12-03 11:06:06 -0300

    att

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ config('app.name') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 
         @livewireStyles
         @vite('resources/css/app.css')
