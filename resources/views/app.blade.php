76dbe94dc201d2f69f7d1b25ae75a44fc729bcd5
Lucas <lucashenriqueslima@outlook.com>
2026-02-11 13:35:32 -0300

    feat: added site

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link
+      rel="icon"
+      type="image/svg+xml"
+      href="https://solidyficando.solidy.org.br/images/logo.png"
+    />
+    <title>Solidyficando Vidas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="manifest" href="/manifest.json" />
+    <title>Solidyficando Vidas</title>
+    <script type="module" crossorigin src="{{ asset('assets/index-DVnNAhXa.js') }}"></script>
+    <link rel="stylesheet" crossorigin href="{{ asset('assets/index-C1NOBDch.css') }}">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
+});
+
+Route::get('/QuemSomos', function () {
+    return view('app');
+});
+
+Route::get('/Eventos', function () {
+    return view('app');
+});
+
+
+
+Route::get('/', function () {
+    return view('app');
 });
 
 Route::get('/doacoes/criar', App\Livewire\CreateDonation::class)->name('donations.create');
